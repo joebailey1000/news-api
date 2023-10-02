f91b4bedda0dbbc8fd5bec0730414f51230ff40f
Joe Bailey <joebailey1000@hotmail.co.uk>
2023-10-02 12:25:01 +0100

    /api/articles/:article_id endpoint added with relevant error handling

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const app=express()
 const {
-    getTopics
+    getTopics,
+    getArticleById
 }=require('./controllers/app.controller')
 
 app.get('/api/healthcheck',(req,res,next)=>{
@@ -10,8 +11,13 @@ app.get('/api/healthcheck',(req,res,next)=>{
 
 app.get('/api/topics',getTopics)
 
-app.use((req,res,err,next)=>{
+app.get('/api/articles/:article_id',getArticleById)
+
+app.use((err,req,res,next)=>{
+    console.log(err.code)
     switch(err.code){
+        case 404: res.status(404).send({msg:'Not found.'})
+        case '22P02': res.status(400).send({msg:'Bad request.'})
         default:res.sendStatus(500)
     }
 })
--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,10 +1,20 @@
 const {
-    fetchAllTopics
+    fetchAllTopics,
+    fetchArticleById
 }=require('../models/app.model')
 
 exports.getTopics=(req,res,next)=>{
     return fetchAllTopics()
         .then((topics)=>{
             res.status(200).send({topics})
+        })
+}
+
+exports.getArticleById=(req,res,next)=>{
+    return fetchArticleById(req.params)
+        .then(article=>{
+            res.status(200).send({article})
+        }).catch(err=>{
+            next(err)
         })
 }
