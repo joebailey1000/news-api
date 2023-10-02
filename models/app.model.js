3d45cb6a0084d495de25daf865bb7310c5c13142
Joe Bailey <joebailey1000@hotmail.co.uk>
2023-10-02 14:06:02 +0100

    removed throw statement in vaour of promise.reject

--- models/app.model.js
+++ models/app.model.js
@@ -11,7 +11,7 @@ exports.fetchArticleById=({article_id})=>{
     return db.query(`SELECT * FROM articles
         WHERE article_id=$1`,[article_id])
         .then(({rows})=>{
-            if (!rows.length) throw {code:404}
+            if (!rows.length) return Promise.reject({code:404})
             return rows[0]
         })
 }
